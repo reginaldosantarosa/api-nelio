df02dad768fe18da3d6c8915ebcd8dc7f205bb2a
Reginaldo Santa Rosa <reginaldo@debian.localhost>
2021-07-23 16:06:57 -0300

    init project

---
+++ src/main/java/com/example/course/CourseNelioApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNelioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNelioApiApplication.class, args);
+	}
+
+}
